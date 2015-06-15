4be7b8efac16e9ebf22f1c218b28fc1ff568e564
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-06-15 22:58:45 +0000

    Bug fixes

--- ansible-playbooks/roles/rpi2/files/ros_arhv7_deb_install.sh
+++ ansible-playbooks/roles/rpi2/files/ros_arhv7_deb_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-if [ $(dpkg-query -W -f='${Status}' nano 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+if [ $(dpkg-query -W -f='${Status}' ros-indogo-ros-base 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
   echo -e "\033[1;32mInstalling ROS...\033[0m"
   sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
   wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
